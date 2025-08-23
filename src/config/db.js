07dd92930aa52e419dd702e26f50a4fd2ae772f8
premkumar <premit.siet@gmail.com>
2025-08-23 10:06:25 +0530

    db changes

--- src/config/db.js
+++ src/config/db.js
@@ -1,6 +1,9 @@
 import knex from 'knex';
-import { development } from '../../knexfile.js';
+import { development, production } from '../../knexfile.js';
 
-const db = knex(development);
+// Decide environment: default to development unless NODE_ENV=production
+const environment = process.env.NODE_ENV === 'production' ? production : development;
+
+const db = knex(environment);
 
 export default db;
